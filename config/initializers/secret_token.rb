72102039922c80485e3292a872d0dc24f50975b3
Robert Jakubowicz <comp353@comp353-VirtualBox.(none)>
2012-07-28 14:21:53 -0400

    created db and added twitter bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rentrack::Application.config.secret_token = '2ca8f01161a646664de05da9754dd69a1b134253d0136577360f0a071ebf87231d05efebeb350cc47905418de92ca0b339ebd4a023f330ffbbe5650ffd9b29d5'
